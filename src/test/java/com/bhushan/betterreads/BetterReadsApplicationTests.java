1399390daa21cf4a5a6bd57b5e93f696ab0f84e5
Ravi Bhushan <ravibhushan122@gmail.com>
2022-02-02 18:02:34 +0530

    initial commit

---
+++ src/test/java/com/bhushan/betterreads/BetterReadsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bhushan.betterreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadsApplicationTests {
+
+    @Test
+    @SuppressWarnings("java:S2699")
+    void contextLoads() {
+    }
+
+}

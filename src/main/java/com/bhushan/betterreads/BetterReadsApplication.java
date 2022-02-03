89db65beb0cadd79b962dd4d8ad69ddac3b26f9d
Ravi Bhushan <ravibhushan122@gmail.com>
2022-02-03 12:36:01 +0530

    configured datastax

--- src/main/java/com/bhushan/betterreads/BetterReadsApplication.java
+++ src/main/java/com/bhushan/betterreads/BetterReadsApplication.java
@@ -1,13 +1,25 @@
 package com.bhushan.betterreads;
 
+import com.bhushan.betterreads.config.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BetterReadsApplication.class, args);
     }
 
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        final Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 }
